56ee36fc1d83f79d98725ceb4690d1e785f0f4ad
MyAntree <dendy.sulanjaya@gmail.com>
2019-11-27 03:10:20 +0600

    send verification mail

--- application/controllers/Verification_email.php
+++ application/controllers/Verification_email.php
@@ -22,7 +22,31 @@ class Verification_email extends CI_Controller {
         $this->load->view('template/common_template', $data);
     }
     public function send_verification_mail(){
+        
+        //Send Password by Email
+        $this->load->library('email');
+
+        $config['protocol'] = 'sendmail';
+        $config['mailpath'] = '/usr/sbin/sendmail';
+        $config['charset'] = 'iso-8859-1';
+        $config['wordwrap'] = TRUE;
+        $this->email->initialize($config);
+
+        $id = $this->input->post('id');
+        
         $data = array();
+        $data['company'] = $company = $this->Common_model->getDataById($id,'tbl_companies');
+        
+        $mesg = $this->load->view('email/verification_email',$data,true);
+        
+
+        $this->email->to('alnurarif@yahoo.com');
+        $this->email->from('alnurarif@gmail.com', 'AlNurArif');
+        $this->email->subject("Verify Account");
+        $this->email->message($mesg);
+        $mail = $this->email->send();
+
+
         $data['main_content'] = $this->load->view('verification_email/verification_mail_sent', $data, TRUE);
         $this->load->view('template/common_template', $data);   
     }
