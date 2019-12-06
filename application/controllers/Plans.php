d5a8703d92a4a2b5103997df993b7a21a4c0d2d2
MyAntree <dendy.sulanjaya@gmail.com>
2019-12-06 16:00:19 +0600

    create response text on paypal ipn test

--- application/controllers/Plans.php
+++ application/controllers/Plans.php
@@ -36,11 +36,9 @@ class Plans extends CI_Controller {
 
         $response = curl_exec($ch);
         curl_close($ch);
-
         // file_put_contents("test.txt", $response);
-
         $this->load->helper('file');
-        if ( ! write_file(base_url().'assets/test.txt', $response))
+        if ( ! write_file(FCPATH.'/assets/test.txt', $response))
         {
         echo 'Unable to write the file';
         }
